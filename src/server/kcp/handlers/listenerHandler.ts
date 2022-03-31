f0c6640c12a65f17d1dcf4fd401c9889e5476ec3
quinella <98860103+quinelladesuu@users.noreply.github.com>
2022-03-31 10:32:58 +0800

    feat(listenerHandler): completed listenerHandler

--- src/server/kcp/handlers/listenerHandler.ts
+++ src/server/kcp/handlers/listenerHandler.ts
@@ -1,10 +1,13 @@
+/* eslint-disable import/no-unresolved */
 /* eslint-disable no-restricted-syntax */
 /* eslint-disable no-unused-vars */
 import { KCP } from 'node-kcp';
 import { RemoteInfo, Socket } from 'dgram';
 import { readdirSync } from 'fs';
 import p from 'path';
-// eslint-disable-next-line import/no-unresolved
+import { LogTypes, Warn } from '../../../utils/logging';
+import { xorData, isValidPacket, parsePacketData } from '../../../utils/dataUtil';
+import { Packet } from '../../../utils/packet';
 import { udpServer } from '../kcpServer';
 
 export default class {
@@ -12,24 +15,40 @@ export default class {
 
   initialKeyBlob!:any;
 
-  keyBlob!:any;
+  xorBlob!:any;
 
   kcpObj!:KCP;
 
-  constructor(kcpObj:KCP) {
+  constructor(kcpObj:KCP, xorBlob:any) {
     this.serverInstance = udpServer;
     this.kcpObj = kcpObj;
+    this.xorBlob = xorBlob;
   }
 
   // eslint-disable-next-line class-methods-use-this
   async init() {
     const listeners = readdirSync(p.join(__dirname, '..', '/listeners/'));
     this.serverInstance.on('message', (msg:Buffer, rinfo:RemoteInfo) => {
-      if (msg === undefined ?? null) return;
       // eslint-disable-next-line guard-for-in
       for (const listener in listeners) {
         (async () => {
-          const l = await import(`../listeners/${listener}`);
+          const l = await import(`../listeners/${listener.split(':')[1]}`);
+          const recv = this.kcpObj.recv();
+          if (recv) {
+            const decrypted = xorData(recv, this.xorBlob);
+            if (isValidPacket(decrypted)) {
+              // eslint-disable-next-line max-len
+              const packet = new Packet(parsePacketData(decrypted), decrypted.readUint16BE(2), this.kcpObj);
+              if (packet.protoName !== undefined) {
+                if (l.protoName === packet.protoName) {
+                  l.exec();
+                } else {
+                  // TO DO: Make Pre-recorded Responses
+                  Warn(`Unhandled packet, ID: ${packet.packetID}`, LogTypes.KCP);
+                }
+              }
+            }
+          }
         })();
       }
     });
