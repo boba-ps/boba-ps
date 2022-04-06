ea1bb235fd57a7adc517d1adfa9a7605c25babab
quinella <98860103+quinelladesuu@users.noreply.github.com>
2022-04-06 20:47:07 +0800

    feat(dbUtils): add some functions for PlayerInfo model

--- src/utils/databaseUtils.ts
+++ src/utils/databaseUtils.ts
@@ -1,5 +1,32 @@
+// TO DO: try-catch incase error
 import { PrismaClient } from '@prisma/client';
+// eslint-disable-next-line import/no-unresolved
+import { PlayerInfo } from './interfaces';
 
 export const prismaClient = new PrismaClient();
 
-// TO DO: DB Stuff;
+export const createPlrData = (plrData:PlayerInfo) => {
+  prismaClient.playerInfo.create({
+    data: plrData,
+  });
+};
+
+export const editPlayerData = (userId:number, newPlrData:PlayerInfo) => {
+  // TO DO: return new playerinfo value
+  prismaClient.playerInfo.update({
+    where: {
+      uid: userId,
+    },
+    data: newPlrData,
+  });
+};
+
+export const findPlayerData = async (userId:number) => {
+  const userData = await prismaClient.playerInfo.findFirst({
+    where: {
+      uid: userId,
+    },
+  });
+
+  return userData;
+};
