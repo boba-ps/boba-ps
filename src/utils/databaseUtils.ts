b2fba2458d13eb38a8c10363ed63d7deac3e14a1
quinella <98860103+quinelladesuu@users.noreply.github.com>
2022-04-18 10:17:35 +0800

    feat(dbUtils): add AvatarInfoUtil

--- src/utils/databaseUtils.ts
+++ src/utils/databaseUtils.ts
@@ -1,18 +1,99 @@
-import { PrismaClient } from '@prisma/client';
+/* eslint-disable max-classes-per-file */
+import { AvatarInfo, PrismaClient } from '@prisma/client';
+import type { PlayerInfo } from './interfaces';
+import { Log } from '../log';
 // Db Access
 export const prismaClient = new PrismaClient();
 
 export class PlayerDataUtil {
-  public uid:number|string = 1;
+  public uid:number = 1;
 
-  constructor(uid:number | string) {
+  constructor(uid:number) {
     this.uid = uid;
   }
 
-  async getPlayerData()  {
+  // eslint-disable-next-line consistent-return
+  async getPlayerData() {
     try {
-      const data = await prismaClient.
+      const data = await prismaClient.playerInfo.findFirst({
+        where: {
+          uid: this.uid,
+        },
+      });
+      if (data !== null) {
+        return data;
+      }
+      return null;
+    } catch (err) {
+      Log.warn(`Cannot get data from uid | ${this.uid}`);
     }
   }
 
+  // eslint-disable-next-line consistent-return
+  async editPlayerData(playerInfo:PlayerInfo) {
+    try {
+      const data = await prismaClient.playerInfo.update({
+        where: {
+          uid: this.uid,
+        },
+        data: playerInfo,
+      });
+      if (data !== null) {
+        return data;
+      }
+      return null;
+    } catch {
+      Log.warn(`Cannot update data from uid | ${this.uid}`);
+    }
+  }
+}
+
+export class AvatarInfoUtil {
+  public uid:number = 1;
+
+  public avatarInfos?:AvatarInfo[] | undefined | null;
+
+  constructor(uid:number) {
+    this.uid = uid;
+    (async () => {
+      this.avatarInfos = await this.getAvatarDatasFromUid();
+    })();
+  }
+
+  // eslint-disable-next-line consistent-return
+  async getAvatarDatasFromUid() {
+    try {
+      const data = await prismaClient.avatarInfo.findMany({
+        where: {
+          uid: this.uid,
+        },
+      });
+
+      if (data !== null) {
+        return data;
+      }
+      return null;
+    } catch {
+      Log.warn(`Cannot update data from uid | ${this.uid}`);
+    }
+  }
+
+  // eslint-disable-next-line consistent-return
+  async updateAvatarData(avatarGuid:number, avatarInfo:AvatarInfo) {
+    try {
+      const data = await prismaClient.avatarInfo.update({
+        // gonna ts ignore this first cuz yes
+        // @ts-ignore
+        where: { guid: avatarGuid },
+        data: avatarInfo,
+      });
+
+      if (data !== null) {
+        return data;
+      }
+      return undefined;
+    } catch {
+      Log.warn(`Cannot update data from uid | ${this.uid}`);
+    }
+  }
 }
