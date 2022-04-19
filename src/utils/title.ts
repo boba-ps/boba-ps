7779bdd968788caf3e975d767f5f7dda0c0d8bf7
phosphene47 <phosphene47@chiya.dev>
2022-04-19 15:16:31 +1000

    Update title.ts

--- src/utils/title.ts
+++ src/utils/title.ts
@@ -1,21 +1,27 @@
 import chalk from "chalk";
 import os from "os";
+import { Executor, ServiceBase } from "../system";
 
 const bobaColor = "#f9d6b8";
 const textColor = "#fdb25a";
 
-export function printTitle() {
-  const mainCpu = os.cpus()[0];
-  const cpuModel = mainCpu?.model.split(" ") || [];
-  const cpuInfo = `${cpuModel[0]} ${os.cpus().length} cores ` + `@ ${(mainCpu?.speed || 0) / 1000}GHz `;
+export class TitlePrinter extends ServiceBase<Executor> {
+  protected setup(exec: Executor) {
+    exec.once(this.print.bind(this));
+  }
 
-  const version = chalk.hex(bobaColor)("| BobaPS ") + chalk.hex(textColor)("v1.0");
-  const authors = chalk.hex(bobaColor)("| With ♡ by ") + chalk.hex(textColor)("quinella, na.na & phosphene47");
+  print() {
+    const mainCpu = os.cpus()[0];
+    const cpuModel = mainCpu?.model.split(" ") || [];
+    const cpuInfo = `${cpuModel[0]} ${os.cpus().length} cores ` + `@ ${(mainCpu?.speed || 0) / 1000}GHz `;
 
-  const cpu = chalk.hex(bobaColor).bold("| CPU: ") + cpuInfo;
-  const ram = `${chalk.hex(bobaColor).bold("| RAM: ") + Math.floor(os.totalmem() / 1000000000)}GB`;
+    const version = chalk.hex(bobaColor)("| BobaPS ") + chalk.hex(textColor)("v1.0");
+    const authors = chalk.hex(bobaColor)("| With ♡ by ") + chalk.hex(textColor)("quinella, na.na & phos");
 
-  console.log(chalk.hex(bobaColor)`
+    const cpu = chalk.hex(bobaColor).bold("| CPU: ") + cpuInfo;
+    const ram = `${chalk.hex(bobaColor).bold("| RAM: ") + Math.floor(os.totalmem() / 1000000000)}GB`;
+
+    console.log(chalk.hex(bobaColor)`
 
           ░░
           ░░
@@ -34,4 +40,5 @@ export function printTitle() {
      ░░░░░░░░░░░░
 
 ——————————————————————————————————————————————————`);
+  }
 }
